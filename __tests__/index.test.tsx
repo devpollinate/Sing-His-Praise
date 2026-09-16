e759de8b89461d338cd99d4b7b8238a184790fd1
rex-makusia <rmakusia@alumni.dwu.ac.pg>
2026-09-17 06:43:21 +1000

    Clean up the project

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react-native";
 import React from "react";
-import Index from "./index";
+import Index from "../app/index";
 
 describe("Index screen", () => {
   it("renders the welcome text", () => {
